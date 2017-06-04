e1b351b8952ff89d793e5168fa887bdfc4c7c526
Luis <luis.f.blanco@gmail.com>
2017-06-04 11:10:20 -0500

    fiest commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'signalr';
+import 'jquery';
+
+import "rxjs/add/operator/map";
 
 import { AppComponent } from './app.component';
 import { TaskStreamComponent } from './features/tasks/task-stream/task-stream.component';
+
+import { ChannelService, ChannelConfig, SignalrWindow } from './services/channel.service';
+
+let channelConfig = new ChannelConfig();
+channelConfig.url = "http://localhost:9123/signalr";
+channelConfig.hubName = "EventHub";
 
 @NgModule({
   declarations: [
@@ -16,7 +26,11 @@ import { TaskStreamComponent } from './features/tasks/task-stream/task-stream.co
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    ChannelService,
+    { provide: SignalrWindow, useValue: window },
+    { provide: 'channel.config', useValue: channelConfig }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/features/tasks/task-stream/task-stream.component.ts
+++ src/app/features/tasks/task-stream/task-stream.component.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import  'signalr'
 
 import { ChannelService, ChannelEvent } from './../../../services/channel.service';
 
